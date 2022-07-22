3872bb0655b8cd5822f290df8dd3951c08f79fc8
bangdrie <107682068+bangdrie@users.noreply.github.com>
2022-07-23 01:12:44 +0700

    Update bootstrap_multi.sh

--- bootstrap_multi.sh
+++ bootstrap_multi.sh
@@ -7,15 +7,10 @@
 # This script is intended to be ran from the multipool installer
 #########################################################
 
-if [ -z "${TAG}" ]; then
-	TAG=v1.52
-fi
-
 # Clone the MultiPool repository if it doesn't exist.
 if [ ! -d $HOME/multipool/yiimp_multi ]; then
-	echo Downloading MultiPool YiiMP Multi Server Installer ${TAG}. . .
+	echo Downloading MultiPool YiiMP Multi Server Installer . .
 	git clone \
-		-b ${TAG} --depth 1 \
 		https://github.com/bangdrie/multipool_yiimp_multi \
 		$HOME/multipool/yiimp_multi \
 		< /dev/null 2> /dev/null
